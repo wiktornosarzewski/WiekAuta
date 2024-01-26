ef61f1e4bca8a5b0b75b4338dae4028176b981a2
Wiktor Nosarzewski <wiktorn2003@gmail.com>
2024-01-26 23:31:55 +0100

    v1
    wersja 1

---
+++ 001_PierwszaKlasa/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_PierwszaKlasa
+{
+    class Car
+    {
+        private String name;
+        private int year;
+
+        public string Name { get => name; set => name=value; }
+        public int Year { get => year; set => year=value; }
+        
+        public int Age
+        {
+            get
+            {
+                return DateTime.Now.Year - this.year;
+            }
+        }
+    }
+}
---
+++ 001_PierwszaKlasa/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace _001_PierwszaKlasa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+
+            Console.WriteLine("Podaj markę: ");
+            c.Name = Console.ReadLine();
+            //c.Year = Int32.Parse( Console.ReadLine());
+            //c.Year = Convert.ToInt32(Console.ReadLine());
+            
+            int tmp;
+            Console.WriteLine("Podaj rocznik: ");
+            while (! Int32.TryParse(Console.ReadLine(), out tmp))
+            {
+                Console.WriteLine("Podaj rocznik: ");
+            }
+            c.Year = tmp;
+            System.Console.WriteLine($"Wiek: {c.Age}");
+            Console.ReadLine();
+        }
+
+
+    }
+}
